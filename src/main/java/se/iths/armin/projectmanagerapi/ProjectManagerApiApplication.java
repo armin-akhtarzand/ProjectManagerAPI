b1ad0aaa4856252b185ed5c32230138b6178cbfe
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-03-19 11:59:31 +0100

    Initial commit, project setup

---
+++ src/main/java/se/iths/armin/projectmanagerapi/ProjectManagerApiApplication.java
@@ -0,0 +1,13 @@
+package se.iths.armin.projectmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerApiApplication.class, args);
+    }
+
+}
