acc1eb45de7e262c36eff0cdfa22a82350f38cb8
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-07 11:47:35 +0200

    feat(ProjectMapper): implement ProjectMapper from EntityMapper

---
+++ src/main/java/se/iths/armin/projectmanagerapi/mapper/ProjectMapperImpl.java
@@ -0,0 +1,41 @@
+package se.iths.armin.projectmanagerapi.mapper;
+
+import se.iths.armin.projectmanagerapi.dto.ProjectRequestDto;
+import se.iths.armin.projectmanagerapi.dto.ProjectResponseDto;
+import se.iths.armin.projectmanagerapi.entity.Project;
+
+public class ProjectMapperImpl
+        implements EntityMapper<Project, ProjectRequestDto, ProjectResponseDto> {
+    @Override
+    public Project toEntity(ProjectRequestDto projectRequestDto) {
+        if (projectRequestDto == null) {
+            return null;
+        }
+        Project project = new Project();
+        project.setTitle(projectRequestDto.title());
+        project.setDescription(projectRequestDto.description());
+
+
+        return project;
+    }
+
+    @Override
+    public ProjectResponseDto toDto(Project project) {
+        if (project == null) {
+            return null;
+        }
+        ProjectResponseDto responseDto = new ProjectResponseDto
+                (project.getTitle(), project.getDescription(), project.getProjectStatus(), project.getCreatedAt());
+
+        return responseDto;
+    }
+
+    @Override
+    public void updateEntity(Project project, ProjectRequestDto projectRequestDto) {
+        if (project == null) {
+            return;
+        }
+        project.setTitle(projectRequestDto.title());
+        project.setDescription(projectRequestDto.description());
+    }
+}
