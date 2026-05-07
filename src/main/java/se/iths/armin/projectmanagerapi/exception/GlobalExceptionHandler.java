52b2807e537794c4684d697b4a8c7bf7b189913f
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-07 11:27:50 +0200

    feat: add ResponseEntity exception handler for correct Status code

--- src/main/java/se/iths/armin/projectmanagerapi/exception/GlobalExceptionHandler.java
+++ src/main/java/se/iths/armin/projectmanagerapi/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package se.iths.armin.projectmanagerapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,12 +12,13 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(DuplicateFoundException.class)
-    public String handleDuplicateFoundException(DuplicateFoundException ex) {
-        return ex.getMessage();
+    public ResponseEntity<?> handleDuplicateFoundException(DuplicateFoundException ex) {
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
-    
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String handleValidationException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException exception) {
         FieldError firstError = exception.getBindingResult().getFieldErrors().stream()
                 .findFirst()
                 .orElse(null);
@@ -23,6 +26,12 @@ public class GlobalExceptionHandler {
                 ? firstError.getDefaultMessage()
                 : exception.getMessage();
 
-        return message;
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception exception) {
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 }
