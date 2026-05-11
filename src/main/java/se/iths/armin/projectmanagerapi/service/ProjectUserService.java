213bb78d4334c2d2fd3357ca2f97cfe21ff21a5f
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-11 11:34:03 +0200

    feat: create ProjectUserService

--- src/main/java/se/iths/armin/projectmanagerapi/service/ProjectUserService.java
+++ src/main/java/se/iths/armin/projectmanagerapi/service/ProjectUserService.java
@@ -1,5 +1,18 @@
 package se.iths.armin.projectmanagerapi.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import se.iths.armin.projectmanagerapi.repository.AppUserRepository;
+import se.iths.armin.projectmanagerapi.repository.ProjectRepository;
+import se.iths.armin.projectmanagerapi.repository.ProjectUserRepository;
 
+@Service
+@RequiredArgsConstructor
 public class ProjectUserService {
+
+    private final ProjectUserRepository projectUserRepository;
+    private final ProjectRepository projectRepository;
+    private final AppUserRepository appUserRepository;
+
+
 }
