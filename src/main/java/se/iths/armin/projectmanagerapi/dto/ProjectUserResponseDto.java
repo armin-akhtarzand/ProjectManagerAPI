feb7fb44640b9a579a42c8f48731db6a7557a819
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-11 11:47:28 +0200

    feat(ProjectUserDto): add request & response dto for ProjectUser

---
+++ src/main/java/se/iths/armin/projectmanagerapi/dto/ProjectUserResponseDto.java
@@ -0,0 +1,16 @@
+package se.iths.armin.projectmanagerapi.dto;
+
+import se.iths.armin.projectmanagerapi.entity.enums.ProjectRole;
+
+import java.time.LocalDateTime;
+
+public record ProjectUserResponseDto(
+        Long projectId,
+        String projectName,
+        Long userId,
+        String firstName,
+        String lastName,
+        ProjectRole role,
+        LocalDateTime joinedAt
+) {
+}
