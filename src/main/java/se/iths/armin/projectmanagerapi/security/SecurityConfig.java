941468da5bce99f44a85ba591160c8ed04d71306
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-23 11:59:20 +0200

    refactor: match requestmatchers with new enpoints

--- src/main/java/se/iths/armin/projectmanagerapi/security/SecurityConfig.java
+++ src/main/java/se/iths/armin/projectmanagerapi/security/SecurityConfig.java
@@ -70,14 +70,14 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/appusers").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/appusers/create").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/appusers/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PATCH, "/appusers/*/position").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/users/*/position").hasRole("ADMIN")
                         .requestMatchers("/auth/login", "/auth/jwks").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/appusers/**").authenticated()
+                        .requestMatchers("/users/**").authenticated()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt ->
                         jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
