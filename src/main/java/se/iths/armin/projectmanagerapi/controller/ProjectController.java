cf8076e294e82bf5be023c4ef28764895704fca4
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-06-15 11:22:24 +0200

    feat: add project CRUD endpoints

--- src/main/java/se/iths/armin/projectmanagerapi/controller/ProjectController.java
+++ src/main/java/se/iths/armin/projectmanagerapi/controller/ProjectController.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import se.iths.armin.projectmanagerapi.dto.project.ChangeProjectStatusDto;
+import se.iths.armin.projectmanagerapi.dto.project.ProjectRequestDto;
 import se.iths.armin.projectmanagerapi.dto.project.ProjectResponseDto;
 import se.iths.armin.projectmanagerapi.service.ProjectService;
 
@@ -23,6 +24,43 @@ public class ProjectController {
         List<ProjectResponseDto> responseDto = projectService.getAllProjects();
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProjectResponseDto> getProjectById(@PathVariable Long id) {
+        ProjectResponseDto responseDto = projectService.findProjectById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProjectResponseDto> saveProject(@RequestBody ProjectRequestDto projectRequestDto) {
+        ProjectResponseDto project = projectService.createProject(projectRequestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(project);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProjectResponseDto> updateProject(@RequestBody ProjectRequestDto projectRequestDto,
+                                                            @PathVariable Long id) {
+        ProjectResponseDto project = projectService.updateProject(projectRequestDto, id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(project);
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Void> patchProjectStatus(@RequestBody ChangeProjectStatusDto changeProjectStatusDto,
+                                                   @PathVariable Long id) {
+        projectService.changeProjectStatus(id, changeProjectStatusDto);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
+        projectService.deleteProjectById(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 
